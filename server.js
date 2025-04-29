9e39997d595cc441dd671c1f2ae4925d7616fc8c
Jasper Green <99476285+mcl-playz@users.noreply.github.com>
2025-04-29 09:26:30 +0800

    Removed line related to nginx proxy

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const hbs = exphb.create({
 app.engine("handlebars", hbs.engine)
 app.set("view engine", "handlebars")
 app.use(express.static("public"));
-app.set('trust proxy', true);
 
 const groupedActs = {}
 const groupedActsIF = {}
